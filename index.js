6fc99c011933e71aac7adaa3d1133008438a529f
Matheus Alves <matttalves@gmail.com>
2020-12-05 21:42:46 -0300

    fix: routes

--- index.js
+++ index.js
@@ -3,4 +3,9 @@ const app = express();
 const port = process.env.port || 3000;
 
 app.use(express.static(__dirname + '/dist'));
+
+app.get('/', function(req, res) {
+    res.render('index.html');
+});
+
 app.listen(port, () => console.log(`Running at localhost:${port}`));
